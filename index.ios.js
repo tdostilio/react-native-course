55f2194d40c654e2fb5151903cb034b76f14d508
tdostilio <tdostilio@gmail.com>
2017-09-19 13:34:08 -0400

    added styling to header

--- index.ios.js
+++ index.ios.js
@@ -2,14 +2,14 @@
 
 // Import a library to help create a component
 import React from 'react';
-import { Text, AppRegistry } from 'react-native';
+import { AppRegistry } from 'react-native';
 import Header from './src/components/header';
 
 // Create a component
 const App = () => (
-  <Header />
+  <Header headerText={'Albums'} />
 );
 
 
 // Render it to the device
-AppRegistry.registerComponent('albums', ()=> App);
+AppRegistry.registerComponent('albums', () => App);
