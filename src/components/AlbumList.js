97a1db3cd3480e05df6022839d9536b48f2c498f
tdostilio <tdostilio@gmail.com>
2017-09-20 11:14:54 -0400

    added axios get request to get album information

---
+++ src/components/AlbumList.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import axios from 'axios';
+
+//This is a functional component - a function that returns JSX
+
+class AlbumList extends Component {
+    state = { albums: [] };
+    componentWillMount() {
+        axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+            .then(response => this.setState({ albums: response.data }));
+    }
+
+    render() {
+        console.log(this.state);
+        return (
+            <View>
+                <Text>Album list!!!</Text>
+            </View>
+        );
+    }
+}
+
+export default AlbumList;
